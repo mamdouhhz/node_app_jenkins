6cd553d88db03a1c82d107c896926fea676d3ec8
mamdouhhz <mamdouhhazemm@gmail.com>
2026-06-29 13:15:58 +0300

    commiting to prod branch

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/submit", async (req, res) => {
             [email, password]
         );
 
-        res.send("تم الاستلام بنجاح prod");
+        res.send("تم الاستلام بنجاح prodd");
     } catch (err) {
         console.error(err);
         res.status(500).send("Database error");
