94c71c1987dc23bedc8be733b24137f6ca0e31ef
mamdouhhz <mamdouhhazemm@gmail.com>
2026-06-29 12:13:27 +0300

    test push

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/submit", async (req, res) => {
             [email, password]
         );
 
-        res.send("Credentials submitted successfullyyyy!!!!!!!!");
+        res.send("Credentials submitted successfully!@!@!");
     } catch (err) {
         console.error(err);
         res.status(500).send("Database error");
