a5aec120143a1130bc264aa777c6a9dab77c48d1
Mamdouh Hazem <110311235+mamdouhhz@users.noreply.github.com>
2026-06-29 11:43:49 +0300

    Fix success message typo in app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/submit", async (req, res) => {
             [email, password]
         );
 
-        res.send("تم الاستلام بنجاححححححح");
+        res.send("تم الاستلام بنجاححح");
     } catch (err) {
         console.error(err);
         res.status(500).send("Database error");
