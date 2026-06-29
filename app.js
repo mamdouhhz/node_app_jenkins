b28d7ab94df8094933e7fff89a89f2fe0dc8bc52
mamdouhhz <mamdouhhazemm@gmail.com>
2026-06-29 10:06:45 +0300

    testing ngrok webhook

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/submit", async (req, res) => {
             [email, password]
         );
 
-        res.send("تم الاستلام بنجاح");
+        res.send("تم الاستلام بنجاحksjdkkjdsj");
     } catch (err) {
         console.error(err);
         res.status(500).send("Database error");
